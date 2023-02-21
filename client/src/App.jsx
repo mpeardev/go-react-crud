533ffa9b45db88399716f3abd65d43a20e302b3f
mirkoperamas <mirkoperamas@gmail.com>
2023-02-20 20:27:21 -0500

    configure proxy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
       <h1>Hello go</h1>
       <button
         onClick={async () => {
-          const response = await fetch("http://localhost:3000/users");
+          const response = await fetch("/users");
           const data = await response.json();
           console.log(data);
         }}
